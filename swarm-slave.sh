e2106dd2fa80723074177318566b4fdb1ee7d7d7
unknown <502392230@HCE-6DCGK12.logon.ds.ge.com>
2015-05-06 09:35:39 +0300

    update startup script

--- swarm-slave.sh
+++ swarm-slave.sh
@@ -4,16 +4,4 @@ if [ ! -f /var/jenkins_home/.ssh/id_rsa ]; then
 	exec ssh-keygen -N '' -f /var/jenkins_home/.ssh/id_rsa
 fi
 
-if [ ! -f ~/slave.jar ]; then
-	if [ -n "$JENKINS_SECRET" ]; then
-		exec wget -O ~/slave.jar https://$MASTER_HOST:$MASTER_PORT/jnlpJars/slave.jar --no-check-certificate
-	else
-		exec wget -O ~/slave.jar http://$MASTER_HOST:$MASTER_PORT/jnlpJars/slave.jar
-	fi
-fi
-
-if [ -n "$JENKINS_SECRET" ]; then
-	exec java -jar ~/slave.jar -jnlpUrl https://$MASTER_HOST:$MASTER_PORT/computer/$SLAVE_NAME/slave-agent.jnlp -secret $JENKINS_SECRET -noCertificateCheck
-else
-	exec java -jar ~/slave.jar -jnlpUrl http://$MASTER_HOST:$MASTER_PORT/computer/$SLAVE_NAME/slave-agent.jnlp
-fi
+exec java -jar /usr/local/lib/swarm-slave.jar -master http://$MASTER_HOST:$MASTER_PORT -labels $JENKINS_LABELS -disableSslVerification -username $JENKINS_USERNAME -password $JENKINS_PASSWORD -fsroot $JENKINS_HOME -mode exclusive 
